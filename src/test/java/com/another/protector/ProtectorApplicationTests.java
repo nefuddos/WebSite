bfe1a529d21fc1fa72f938d7562415ebb3777b9e
denggaofeng <1415356791@qq.com>
2018-05-20 18:37:12 +0800

    初始化测试代码

--- src/test/java/com/another/protector/ProtectorApplicationTests.java
+++ src/test/java/com/another/protector/ProtectorApplicationTests.java
@@ -8,9 +8,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProtectorApplicationTests {
-
+	/**
+	 * @Test 最基本的注解，用来把方法标记为测试的一部分
+	 */
 	@Test
 	public void contextLoads() {
+		System.out.println("This is the test case 1");
 	}
 
 }
